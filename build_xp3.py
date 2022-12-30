70ecb0a16d0790ab5e506ded2ce2771f0f7e3c99
Joel Niklaus <me@joelniklaus.ch>
2022-12-30 11:12:35 -0800

    changed to xP3mt because we have enough instruction datasets already and should rather prioritize on more languages

--- build_xp3.py
+++ build_xp3.py
@@ -18,11 +18,14 @@ train_f = xz.open(get_output_file_name(category, output_file_idx), "wt")
 _LANG = ['en', 'es', 'fr', 'pt', 'code']
 # TODO maybe treat code as a separate category so we can filter easily
 
+# prompts translated into other languages
+# rather use this one instead of xP3all because we compile multi_eurlex ourselves and xP3all only has two datasets more.
+# We already have enough instruction datasets and rather prioritize using more languages in the prompts
 for lang in _LANG:
     print("############################")
-    print(f"########## xP3all {lang} ###########")
+    print(f"########## bigscience/xP3mt {lang} ###########")
     print("############################")
-    df = load_dataset("bigscience/xP3all", lang)
+    df = load_dataset("bigscience/xP3mt", lang)
     for example in tqdm(df["train"]):
         datapoint = example["inputs"] + " " + example["targets"]
         if os.path.getsize(get_output_file_name(category, output_file_idx)) > MAX_FILE_SIZE:
