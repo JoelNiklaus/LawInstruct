ccc656d0e270b3d4fde5a1bc4b108933053c3e81
Arya McCarthy <arya.mccarthy@gmail.com>
2023-02-09 16:32:58 +0000

    Document abstract dataset fields

--- abstract_dataset.py
+++ abstract_dataset.py
@@ -1,3 +1,4 @@
+"""Types that are used for all datasets."""
 import datetime
 import enum
 import json
@@ -7,7 +8,7 @@ import random
 import sys
 from collections.abc import Iterator
 from pathlib import Path
-from typing import Any, TextIO
+from typing import Any, TextIO, TypedDict
 
 from tqdm import tqdm
 
@@ -97,9 +98,35 @@ class JURISDICTION(_AutoName):
     N_A = enum.auto()  # Not a legal task
 
 
+class DataPoint(TypedDict):
+    """A data point in the dataset for training an LLM."""
+    prompt_language: str
+    answer_language: str
+    text: str
+    task_type: TASK_TYPE
+    jurisdiction: JURISDICTION
+    subset: str
+
+
 class AbstractDataset:
+    """Dataset class that should be subclassed by all datasets.
+
+    This class provides a few helper methods to make it easier to implement a
+    new dataset.
+
+    Attributes:
+        name: The name of the dataset.
+        source: The source of the dataset, e.g. a URL.
+        data_dir: The directory where the dataset should be written out.
+        random: A random.Random instance that should be used to make the dataset
+            reproducible.
+        raw_data_dir: The directory where any raw data is found.
+        logger: A logger instance that should be used to log information.
+    """
 
     def __init__(self, name: str, source: str, data_dir: os.PathLike = "data"):
+        if " " in name:
+            raise ValueError("Dataset name should not contain spaces.")
         self.name = name
         self.source = source
         self.data_dir = data_dir
@@ -108,6 +135,12 @@ class AbstractDataset:
 
         self.raw_data_dir = "lawinstruct_raw/raw_data"
 
+        self._configure_logging()
+        self.logger = logging.getLogger(__name__)
+
+    @staticmethod
+    def _configure_logging() -> None:
+        """Configures the logging module to log to stdout."""
         root = logging.getLogger()
         root.setLevel(logging.INFO)
         handler = logging.StreamHandler(sys.stdout)
@@ -116,12 +149,11 @@ class AbstractDataset:
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
         root.addHandler(handler)
-        self.logger = logging.getLogger(__name__)
 
     def get_data(self) -> Iterator[dict]:
         raise NotImplementedError(
-            "This method should yield datapoint dicts with the following keys: "
-            "prompt_language, answer_language, task_type, jurisdiction, text")
+            "This method should yield DataPoint dicts with the following keys: "
+            f"{', '.join(DataPoint.__annotations__.keys())}.")
 
     def get_instruction_bank(self, language="en"):
         return json.loads(
@@ -133,7 +165,20 @@ class AbstractDataset:
                          text: str,
                          task_type: TASK_TYPE = TASK_TYPE.UNKNOWN,
                          jurisdiction: JURISDICTION = JURISDICTION.UNKNOWN,
-                         subset: str = "") -> dict:
+                         subset: str = "") -> DataPoint:
+        """Builds a data point.
+
+        Args:
+            prompt_language: The language of the prompt.
+            answer_language: The language of the answer.
+            text: The text of the prompt and answer.
+            task_type: The type of the task.
+            jurisdiction: The jurisdiction of the task.
+            subset: The subset of the dataset the datapoint belongs to.
+
+        Returns:
+            A data point with the given attributes.
+        """
         del self  # We don't use `self`, but subclasses might.
         return {
             "prompt_language": prompt_language,
@@ -144,8 +189,18 @@ class AbstractDataset:
             "subset": subset,
         }
 
-    def write_json_line(self, file: TextIO, datapoint: dict) -> None:
-        assert datapoint['text'], "datapoint['text'] must not be empty"
+    def write_json_line(self, file: TextIO, datapoint: DataPoint) -> None:
+        """Write a datapoint to a file in JSON format.
+
+        Args:
+            file: The file to write to.
+            datapoint: The datapoint to write.
+        """
+        if not datapoint['text']:
+            raise ValueError(
+                f"datapoint['text'] must not be empty in {datapoint}")
+        # text is last, so we can easily read the metadata (on servers, for
+        # example)
         file.write(
             json.dumps({
                 "dataset_name":
@@ -165,22 +220,38 @@ class AbstractDataset:
                 "downloaded_timestamp":
                 datetime.date.today().strftime("%m-%d-%Y"),
                 "text":
-                datapoint[
-                    'text'],  # text is last, so we can easily read the metadata on servers for example
+                datapoint['text'],
             }) + "\n")
 
     def get_output_file_name(self,
                              file_idx: int = 0,
                              split: str = 'train') -> str:
-        # we save each dataset to a separate file, so we only need to generate new datasets
+        """Builds an output file name.
+
+        Args:
+            file_idx: An increasing counter for files of this dataset.
+            split: The split of the dataset, e.g. train, dev, test.
+        Returns:
+            The output file name.
+        """
+        # we save each dataset to a separate file, so we only need to generate
+        # new datasets
         return f"{self.data_dir}/{self.name}.{split}.{file_idx}.jsonl.xz"
 
-    def build_instruction_dataset(self, debug_size=-1) -> None:
+    def build_instruction_dataset(self, debug_size: int = -1) -> None:
+        """Writes a dataset to files.
+
+        Args:
+            debug_size: If > 0, only write this many datapoints.
+        """
         output_file_idx = 0
         file = self.open_new_file(output_file_idx)
         self.logger.info(f"Building instruction dataset for {self.name}")
         count = 0
         for datapoint in tqdm(self.get_data()):
+            # TODO(arya): This checks the file size for every datapoint. We
+            #   should check the file size only after a certain number of
+            #   datapoints.
             if os.path.getsize(self.get_output_file_name(
                     output_file_idx)) > MAX_FILE_SIZE:
                 file.close()
@@ -188,7 +259,7 @@ class AbstractDataset:
                 file = self.open_new_file(output_file_idx)
             self.write_json_line(file, datapoint)
             count += 1
-            if debug_size > 0 and count >= debug_size:
+            if 0 < debug_size <= count:
                 self.logger.info(f"Stopping after {debug_size} datapoints")
                 self.logger.info(f"Example datapoint: {datapoint}")
                 break
@@ -196,6 +267,13 @@ class AbstractDataset:
         file.close()
 
     def open_new_file(self, output_file_idx: int) -> TextIO:
+        """Opens a new file for writing.
+
+        Args:
+            output_file_idx: The index of the file to open.
+        Returns:
+            The file object.
+        """
         filename = self.get_output_file_name(output_file_idx)
         self.logger.info(f"Writing to {filename}")
         return xz.open(filename, "wt")  # do we need append mode here?
