a7531a92f9bf95a0986b57b7053fe60373b349b2
Arya McCarthy <arya.mccarthy@gmail.com>
2023-03-29 18:19:06 +0100

    Update jec_qa.py

--- instruction_datasets/jec_qa.py
+++ instruction_datasets/jec_qa.py
@@ -18,6 +18,7 @@ class JECQA(AbstractDataset):
         ]
         task_type = TaskType.QUESTION_ANSWERING
         jurisdiction = Jurisdiction.CHINA
+        instruction_language = "en"
         prompt_language = "en"
 
         with open(f"{self.raw_data_dir}/jecqa_0_train.json") as f:
@@ -30,6 +31,7 @@ class JECQA(AbstractDataset):
             prompt = f"{q['statement']}\n\n"
             for k, v in q["option_list"].items():
                 prompt += f"{k}. {v}\n"
-            prompt += "\n\nFinal Answer(s): {','.join(q['answer'])}"
-            yield self.build_data_point(prompt_language, "zh", instruction,
-                                        prompt, task_type, jurisdiction)
+            prompt.rstrip("\n")  # remove trailing newline
+            answer = "Final Answer(s): {','.join(q['answer'])}"
+            yield self.build_data_point(instruction_language, prompt_language, "zh", instruction,
+                                        prompt, answer, task_type, jurisdiction)
