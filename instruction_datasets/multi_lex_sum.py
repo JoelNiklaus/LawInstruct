fcf0fc9a95c7b2d1c7b40b920b63bc809bd15c75
Arya McCarthy <arya.mccarthy@gmail.com>
2023-03-29 17:38:56 +0100

    Update multi_lex_sum.py

--- instruction_datasets/multi_lex_sum.py
+++ instruction_datasets/multi_lex_sum.py
@@ -1,3 +1,5 @@
+import string
+
 from datasets import load_dataset
 
 from abstract_dataset import AbstractDataset
@@ -5,8 +7,14 @@ from enums import Jurisdiction
 from enums import TaskType
 
 
-def build_summarization_answer(input, summary):
-    return f"Passage: {input}. Summary: {summary}"
+def build_summarization_answer(input: str, summary: str) -> tuple[str, str]:
+    prompt = f"Passage: {input}"
+    # Add a period if the last character is not a punctuation.
+    if prompt[-1] not in string.punctuation:
+        prompt += "."
+
+    answer = f"Summary: {summary}"
+    return prompt, answer
 
 
 class MultiLexSum(AbstractDataset):
@@ -20,6 +28,7 @@ class MultiLexSum(AbstractDataset):
         df = load_dataset("allenai/multi_lexsum", split="train")
         task_type = TaskType.SUMMARIZATION
         jurisdiction = Jurisdiction.US
+        instruction_language = "en"
         prompt_language = "en"
 
         instruction_bank = [
@@ -31,19 +40,19 @@ class MultiLexSum(AbstractDataset):
             if example["summary/short"]:
                 summary = example["summary/short"]
                 instruction = self.random.choice(instruction_bank)
-                text = build_summarization_answer(input, summary)
-                yield self.build_data_point(prompt_language, "en", instruction,
-                                            text, task_type, jurisdiction)
+                prompt, answer = build_summarization_answer(input, summary)
+                yield self.build_data_point(instruction_language, prompt_language, "en", instruction,
+                                            prompt, answer, task_type, jurisdiction)
             if example["summary/tiny"]:
                 summary = example["summary/tiny"]
                 instruction = self.random.choice(instruction_bank)
-                text = build_summarization_answer(input, summary)
-                yield self.build_data_point(prompt_language, "en", instruction,
-                                            text, task_type, jurisdiction)
+                prompt, answer = build_summarization_answer(input, summary)
+                yield self.build_data_point(instruction_language, prompt_language, "en", instruction,
+                                            prompt, answer, task_type, jurisdiction)
             if example["summary/short"] and example["summary/tiny"]:
                 input = example["summary/short"]
                 summary = example["summary/tiny"]
                 instruction = self.random.choice(instruction_bank)
-                text = build_summarization_answer(input, summary)
-                yield self.build_data_point(prompt_language, "en", instruction,
-                                            text, task_type, jurisdiction)
+                prompt, answer = build_summarization_answer(input, summary)
+                yield self.build_data_point(instruction_language, prompt_language, "en", instruction,
+                                            prompt, answer, task_type, jurisdiction)
