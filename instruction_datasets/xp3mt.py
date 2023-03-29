83006d531f3a390e390d176f1ba62b7789f8054e
Arya McCarthy <arya.mccarthy@gmail.com>
2023-03-29 16:51:57 +0100

    Update xp3mt.py

--- instruction_datasets/xp3mt.py
+++ instruction_datasets/xp3mt.py
@@ -7,6 +7,10 @@ from enums import Jurisdiction
 from enums import TaskType
 
 
+_BLANK_INSTRUCTION = ''
+_BLANK_INSTRUCTION_LANGUAGE = 'zxx'
+
+
 class XP3MT(AbstractDataset):
 
     def __init__(self):
@@ -20,7 +24,6 @@ class XP3MT(AbstractDataset):
         # Maybe also add 'zh', 'vi', because we have legal instruction datasets there
         _LANG = ['en', 'es', 'fr', 'pt', 'code']
         # TODO maybe treat code as a separate category so we can filter easily
-        _BLANK_INSTRUCTIONS = ""
 
         # prompts translated into other languages
         # rather use this one instead of xP3all because we compile multi_eurlex ourselves and xP3all only has two datasets more.
@@ -28,13 +31,16 @@ class XP3MT(AbstractDataset):
         for lang in _LANG:
             df = load_dataset("bigscience/xP3mt", lang, split="train")
             for example in tqdm(df):
-                text = example["inputs"] + " " + example["targets"]
+                prompt = example["inputs"]
+                answer = example["targets"]
                 task_type = (TaskType.CODE
                              if lang == "code" else TaskType.UNKNOWN)
                 prompt_language = detect(text=example["inputs"].replace(
                     "\n", " "),
                                          low_memory=True)['lang']
                 answer_language = lang if lang != "code" else "en"
-                yield self.build_data_point(prompt_language, answer_language,
-                                            _BLANK_INSTRUCTIONS, text,
+                yield self.build_data_point(
+                    _BLANK_INSTRUCTION_LANGUAGE,
+                    prompt_language, answer_language,
+                                            _BLANK_INSTRUCTION, prompt, answer,
                                             task_type, jurisdiction)
