7d1be887ec3518afe0a5fdca70028afd1c8e8691
Arya McCarthy <arya.mccarthy@gmail.com>
2023-03-29 17:36:05 +0100

    Update olc_memos.py

--- instruction_datasets/olc_memos.py
+++ instruction_datasets/olc_memos.py
@@ -5,6 +5,9 @@ from enums import Jurisdiction
 from enums import TaskType
 
 
+_BLANK_PROMPT = ''
+
+
 class OLCMemos(AbstractDataset):
 
     def __init__(self):
@@ -24,6 +27,7 @@ class OLCMemos(AbstractDataset):
         ]
         task_type = TaskType.QUESTION_ANSWERING
         jurisdiction = Jurisdiction.US
+        instruction_language = "en"
         prompt_language = "en"
 
         for example in df["text"]:
@@ -31,9 +35,13 @@ class OLCMemos(AbstractDataset):
                 example = example.encode().decode('unicode-escape').encode(
                     'latin1').decode('utf-8')[2:-2].strip()
             instruction = self.random.choice(instruction_bank)
-            yield self.build_data_point(prompt_language,
+            # FIXME: The memo topic is part of the text, but it may be multiline.
+            #  There is no clean way to extract it beyond manual inspection.
+            yield self.build_data_point(instruction_language,
+                prompt_language,
                                         "en",
                                         instruction,
+                                        _BLANK_PROMPT,
                                         example,
                                         task_type,
                                         jurisdiction,
