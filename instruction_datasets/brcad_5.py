c2a08ba05146631eac5c8a329d132a05e3478a63
Arya McCarthy <arya.mccarthy@gmail.com>
2023-03-29 18:53:00 +0100

    Update brcad_5.py

--- instruction_datasets/brcad_5.py
+++ instruction_datasets/brcad_5.py
@@ -22,48 +22,52 @@ class BrCAD5(AbstractDataset):
         df = load_dataset('joelito/BrCAD-5', split='train')
         task_type = TaskType.TEXT_CLASSIFICATION
         jurisdiction = Jurisdiction.BRAZIL
+        instruction_language = "en"
         prompt_language = "en"
         answer_language = "pt"
 
         for example in df:
             case = example['preprocessed_full_text_first_instance_court_ruling']
             instruction = 'Determine what you think the Brazilian appeals court will rule for the case.'
-            text = f"Case:{case}\nJudgement: {example['label']}"
-            yield self.build_data_point(prompt_language, answer_language,
-                                        instruction, text, task_type,
+            prompt = f"Case: {case}"
+            answer = f"Judgement: {example['label']}"
+            yield self.build_data_point(instruction_language, prompt_language, answer_language,
+                                        instruction, prompt, answer, task_type,
                                         jurisdiction)
 
             # TODO: this isn't _really_ an instruction, just a question...
             instruction = 'What area of law is this case related to?'
-            text = f"Case:{case}\nArea of Law: {example['current_case_class']}"
-            yield self.build_data_point(prompt_language, answer_language,
-                                        instruction, text, task_type,
+            prompt = f"Case: {case}"
+            answer = f"Area of Law: {example['current_case_class']}"
+            yield self.build_data_point(instruction_language, prompt_language, answer_language,
+                                        instruction, prompt, answer, task_type,
                                         jurisdiction)
 
             for level in ["1st", "2nd", "3rd"]:
                 # TODO: this isn't _really_ an instruction, just a question...
                 instruction = f"What {level}-level topic is this case related to?"
-                text = f"Case:{case}\nTopic: {example[f'case_topic_{level}_level']}"
-                yield self.build_data_point(prompt_language, answer_language,
-                                            instruction, text, task_type,
+                prompt = f"Case: {case}"
+                answer = f"Topic: {example[f'case_topic_{level}_level']}"
+                yield self.build_data_point(instruction_language, prompt_language, answer_language,
+                                            instruction, prompt, answer, task_type,
                                             jurisdiction)
 
             outcome_mc1 = ["(a)", "(b)"][['NÃO PROVIMENTO',
                                           'PROVIMENTO'].index(example["label"])]
             instruction = self.random.choice(
                 get_multiple_choice_instruction_bank())
-            text = f"Question: {case} How would the court find?\n(a) The court should dismiss the case.\n(b) The court should affirm the case.\n" \
-                   f"Answer: {outcome_mc1}."
-            yield self.build_data_point(prompt_language, answer_language,
-                                        instruction, text, task_type,
+            prompt = f"Question: {case} How would the court find?\n(a) The court should dismiss the case.\n(b) The court should affirm the case."
+            answer = f"Answer: {outcome_mc1}."
+            yield self.build_data_point(instruction_language, prompt_language, answer_language,
+                                        instruction, prompt, answer, task_type,
                                         jurisdiction)
 
             outcome_mc1 = ["(b)", "(a)"][['NÃO PROVIMENTO',
                                           'PROVIMENTO'].index(example["label"])]
             instruction = self.random.choice(
                 get_multiple_choice_instruction_bank())
-            text = f"Question: {case} How would the court find?\n(a) The court should approve the case.\n(b) The court should dismiss the case.\n" \
-                   f"Answer: {outcome_mc1}."
-            yield self.build_data_point(prompt_language, answer_language,
-                                        instruction, text, task_type,
+            prompt = f"Question: {case} How would the court find?\n(a) The court should approve the case.\n(b) The court should dismiss the case."
+            answer = f"Answer: {outcome_mc1}."
+            yield self.build_data_point(instruction_language, prompt_language, answer_language,
+                                        instruction, prompt, answer, task_type,
                                         jurisdiction)
