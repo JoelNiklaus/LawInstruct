e4dc88c06bfed7db96c7c19666f4f46ba5e5b4fd
Joel Niklaus <me@joelniklaus.ch>
2023-04-12 10:47:18 +0200

    added canton mapping

--- instruction_datasets/swiss_judgment_prediction.py
+++ instruction_datasets/swiss_judgment_prediction.py
@@ -14,21 +14,56 @@ def get_multiple_choice_instruction_bank():
     ]
 
 
+canton_mapping = {
+    "AG": "Aargau",
+    "AI": "Appenzell Innerrhoden",
+    "AR": "Appenzell Ausserrhoden",
+    "BE": "Bern",
+    "BL": "Basel-Landschaft",
+    "BS": "Basel-Stadt",
+    "FR": "Fribourg",
+    "GE": "Geneva",
+    "GL": "Glarus",
+    "GR": "Graubünden",
+    "JU": "Jura",
+    "LU": "Lucerne",
+    "NE": "Neuchâtel",
+    "NW": "Nidwalden",
+    "OW": "Obwalden",
+    "SG": "St. Gallen",
+    "SH": "Schaffhausen",
+    "SO": "Solothurn",
+    "SZ": "Schwyz",
+    "TG": "Thurgau",
+    "TI": "Ticino",
+    "UR": "Uri",
+    "VD": "Vaud",
+    "VS": "Valais",
+    "ZG": "Zug",
+    "ZH": "Zurich",
+}
+
+
+def get_canton_name(canton_code):
+    return canton_mapping[canton_code.upper()]
+
+
 class SwissJudgmentPrediction(AbstractDataset):
 
     def __init__(self):
         super().__init__(
             "SwissJudgmentPrediction",
-            "https://huggingface.co/datasets/swiss_judgment_prediction")
+            "https://huggingface.co/datasets/rcds/swiss_judgment_prediction")
 
     def get_data(self):
-        df = load_dataset('swiss_judgment_prediction', 'all+mt', split='train')
+        df = load_dataset('rcds/swiss_judgment_prediction', 'all+mt', split='train')
         task_type = TaskType.TEXT_CLASSIFICATION
         jurisdiction = Jurisdiction.SWITZERLAND
         instruction_language = 'en'
         answer_language = "en"
         for example in df:
-            court_location = "" if example['region'] == "n/a" else f"The court is located in {example['region']}."
+            court_location = "" if example['canton'] == "n/a" \
+                else f"The lower court is located in {get_canton_name(example['canton'])}."
             judgement = ["dismiss", "approve"][example['label']]
             instruction = f"Determine if you think the Swiss court will dismiss or approve the case. {court_location}"
             prompt = f"Facts: {example['text']}"
@@ -47,7 +82,7 @@ class SwissJudgmentPrediction(AbstractDataset):
             if court_location != "":
                 instruction = "Where do you think this case was adjudicated?"
                 prompt = f"Case: {example['text']}"
-                answer = f"Region: {example['region']}"
+                answer = f"Canton: {get_canton_name(example['canton'])}. Region: {example['region']}"
                 yield self.build_data_point(instruction_language, example["language"],
                                             answer_language,
                                             instruction,
