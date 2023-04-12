7e207cd6946f0e619e023ea75c0619e74b917bd3
Joel Niklaus <me@joelniklaus.ch>
2023-04-12 09:16:46 +0200

    fixed languages in swiss_judgment_prediction

--- instruction_datasets/swiss_judgment_prediction.py
+++ instruction_datasets/swiss_judgment_prediction.py
@@ -26,32 +26,31 @@ class SwissJudgmentPrediction(AbstractDataset):
         task_type = TaskType.TEXT_CLASSIFICATION
         jurisdiction = Jurisdiction.SWITZERLAND
         instruction_language = 'en'
-        prompt_language = "en"
+        answer_language = "en"
         for example in df:
-            court_location = "" if example[
-                'region'] == "n/a" else f"The court is located in {example['region']}."
+            court_location = "" if example['region'] == "n/a" else f"The court is located in {example['region']}."
             judgement = ["dismiss", "approve"][example['label']]
             instruction = f"Determine if you think the Swiss court will dismiss or approve the case. {court_location}"
             prompt = f"Facts: {example['text']}"
             answer = f"Judgement: {judgement}"
-            yield self.build_data_point(instruction_language, prompt_language,
-                                        example["language"], instruction,
+            yield self.build_data_point(instruction_language, example["language"], answer_language,
+                                        instruction,
                                         prompt, answer, task_type, jurisdiction)
 
             instruction = "What area of law is this case related to?"
             prompt = f"Case: {example['text']}"
             answer = f"Area of Law: {example['legal area']}"
-            yield self.build_data_point(instruction_language, prompt_language,
-                                        example["language"], instruction,
+            yield self.build_data_point(instruction_language, example["language"], answer_language,
+                                        instruction,
                                         prompt, answer, task_type, jurisdiction)
 
             if court_location != "":
                 instruction = "Where do you think this case was adjudicated?"
                 prompt = f"Case: {example['text']}"
                 answer = f"Region: {example['region']}"
-                yield self.build_data_point(instruction_language,
-                                            prompt_language,
-                                            example["language"], instruction,
+                yield self.build_data_point(instruction_language, example["language"],
+                                            answer_language,
+                                            instruction,
                                             prompt, answer, task_type,
                                             jurisdiction)
 
@@ -63,8 +62,8 @@ class SwissJudgmentPrediction(AbstractDataset):
             prompt = f"Question: {text} How would the court find?\n" \
                      f"(a) The court should dismiss the case.\n(b) The court should affirm the case."
             answer = f"Answer: {outcome_mc1}."
-            yield self.build_data_point(instruction_language, prompt_language,
-                                        example["language"], instruction,
+            yield self.build_data_point(instruction_language, answer_language, example["language"],
+                                        instruction,
                                         prompt, answer, task_type, jurisdiction)
 
             outcome_mc1 = ["(b)", "(a)"][example["label"]]
@@ -74,6 +73,6 @@ class SwissJudgmentPrediction(AbstractDataset):
             prompt = f"Question: {text} How would the court find?\n" \
                      f"(a) The court should approve the case.\n(b) The court should dismiss the case."
             answer = f"Answer: {outcome_mc1}."
-            yield self.build_data_point(instruction_language, prompt_language,
-                                        example["language"], instruction,
+            yield self.build_data_point(instruction_language, answer_language, example["language"],
+                                        instruction,
                                         prompt, answer, task_type, jurisdiction)
