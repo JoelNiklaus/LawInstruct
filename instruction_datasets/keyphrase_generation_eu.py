b74230bf6b2a6dff8a2b972a870b4842e9c5be41
Viola Meier <viola.camille.meier@gmail.com>
2024-07-03 14:17:25 +0200

    added seed instructions for keyphrase_generation_eu

--- instruction_datasets/keyphrase_generation_eu.py
+++ instruction_datasets/keyphrase_generation_eu.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 from datasets import load_dataset
 
 from abstract_dataset import AbstractDataset
@@ -5,19 +6,27 @@ from enums import Jurisdiction
 from enums import TaskType
 import instruction_manager
 
+
+def build_answer(keyphrases_list: Sequence[Sequence[str]]) ->str:
+    markdown_list = ""
+    for keyphrases in keyphrases_list:
+        for keyphrase in keyphrases:
+            markdown_list += "- " + keyphrase + "\n"
+    return markdown_list 
+
 class KeyphraseGenerationEU(AbstractDataset): 
     
     def get_data(self, instructions: instruction_manager.InstructionManager): 
-        ds = load_dataset("NCube/europa", split =' train')
-        task_type = TaskType.SUMMARIZATION
+        ds = load_dataset("NCube/europa", split ='train')
+        task_type = TaskType.TEXT_CLASSIFICATION
         jurisdiction = Jurisdiction.EU
         instruction_language: str
         for example in ds:
             subset = 'keyphrase_generation_eu'
             instruction, instruction_language = instructions.sample(subset)
-            promt = f"Judgment transcription: {example['input_text']}"
-            answer = f"Keyphrases : {example['keyphrases']}"
+            prompt = f"Judgment transcription: {example['input_text']}"
+            answer = f"Keyphrases: {build_answer(example['keyphrases'])}"
             yield self.build_data_point(instruction_language, example['lang'],
                                         example['lang'], instruction,
-                                        promt, answer, task_type, 
+                                        prompt, answer, task_type, 
                                         jurisdiction, subset)
